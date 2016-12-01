62b419d08178a40bb8c2663df6cc2b7359e470c4
Darren Ehlers <me@darrenehlers.com>
2016-12-01 09:44:50 -0600

    Added WKRCrash_MediaLibrary_Worker and updated podspec version
    Signed-off-by: Darren Ehlers <me@darrenehlers.com>

---
+++ Classes/WKRCrash_MediaLibrary_Worker.h
@@ -0,0 +1,13 @@
+//
+//  WKRCrash_MediaLibrary_Worker.h
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#import <DNCProtocols/PTCLMediaLibrary_Protocol.h>
+
+@interface WKRCrash_MediaLibrary_Worker : NSObject<PTCLMediaLibrary_Protocol>
+
+@end
---
+++ Classes/WKRCrash_MediaLibrary_Worker.m
@@ -0,0 +1,114 @@
+//
+//  WKRCrash_File_Worker.m
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#import <Photos/PHImageManager.h>
+
+#import "WKRCrash_MediaLibrary_Worker.h"
+
+@interface WKRCrash_MediaLibrary_Worker ()
+
+@end
+
+@implementation WKRCrash_MediaLibrary_Worker
+
+@synthesize nextBaseWorker;
+@synthesize nextMediaLibraryWorker;
+
++ (instancetype _Nonnull)worker   {   return [self worker:nil]; }
+
++ (instancetype _Nonnull)worker:(nullable id<PTCLMediaLibrary_Protocol>)nextMediaLibraryWorker
+{
+    id<PTCLMediaLibrary_Protocol> worker    = [[self.class alloc] init];
+    worker.nextMediaLibraryWorker           = nextMediaLibraryWorker;
+    return worker;
+}
+
+- (void)enableOption:(nonnull NSString*)option
+{
+    if (self.nextMediaLibraryWorker)
+    {
+        [self.nextMediaLibraryWorker enableOption:option];
+    }
+    
+    // Options not used in this Worker
+}
+
+- (void)disableOption:(nonnull NSString*)option
+{
+    if (self.nextMediaLibraryWorker)
+    {
+        [self.nextMediaLibraryWorker disableOption:option];
+    }
+    
+    // Options not used in this Worker
+}
+
+#pragma mark - Business Logic
+
++ (CGSize)PTCLMediaLibraryMaximumSize
+{
+    return PHImageManagerMaximumSize;
+}
+
+- (BOOL)doCheckAuthorization
+{
+    if (self.nextMediaLibraryWorker)
+    {
+        [self.nextMediaLibraryWorker doCheckAuthorization];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+- (void)doRequestAuthorizationWithCompletionBlock:(nullable PTCLMediaLibraryBlockVoidNSError)completionBlock
+{
+    if (self.nextMediaLibraryWorker)
+    {
+        [self.nextMediaLibraryWorker doRequestAuthorizationWithCompletionBlock:completionBlock];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+- (void)doLoadImagesWithCompletionBlock:(nullable PTCLMediaLibraryBlockVoidNSArrayNSError)completionBlock
+{
+    if (self.nextMediaLibraryWorker)
+    {
+        [self.nextMediaLibraryWorker doLoadImagesWithCompletionBlock:completionBlock];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+- (void)doLoadImage:(nonnull id)asset
+               size:(CGSize)size
+    completionBlock:(nullable PTCLMediaLibraryBlockVoidUIImageNSDictionary)completionBlock
+{
+    if (self.nextMediaLibraryWorker)
+    {
+        [self.nextMediaLibraryWorker doLoadImage:asset
+                                            size:size
+                                 completionBlock:completionBlock];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+@end
