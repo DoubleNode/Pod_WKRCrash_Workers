7f5f02d961fc3dfd06aa407744f3a9bbdbddf0f3
Darren Ehlers <me@darrenehlers.com>
2016-12-06 12:14:15 -0600

    Added UserDevice worker and updated podspec version
    Signed-off-by: Darren Ehlers <me@darrenehlers.com>

---
+++ Classes/WKRCrash_UserDevice_Worker.m
@@ -0,0 +1,138 @@
+//
+//  WKRCrash_UserDevice_Worker.m
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#import "WKRCrash_UserDevice_Worker.h"
+
+@interface WKRCrash_UserDevice_Worker ()
+
+@end
+
+@implementation WKRCrash_UserDevice_Worker
+
+@synthesize nextBaseWorker;
+@synthesize nextUserDeviceWorker;
+
++ (instancetype _Nonnull)worker   {   return [self worker:nil]; }
+
++ (instancetype _Nonnull)worker:(nullable id<PTCLUserDevice_Protocol>)nextUserDeviceWorker
+{
+    id<PTCLUserDevice_Protocol> worker  = [[self.class alloc] init];
+    worker.nextUserDeviceWorker         = nextUserDeviceWorker;
+    return worker;
+}
+
+- (void)enableOption:(nonnull NSString*)option
+{
+    if (self.nextUserDeviceWorker)
+    {
+        [self.nextUserDeviceWorker enableOption:option];
+    }
+    
+    // Options not used in this Worker
+}
+
+- (void)disableOption:(nonnull NSString*)option
+{
+    if (self.nextUserDeviceWorker)
+    {
+        [self.nextUserDeviceWorker disableOption:option];
+    }
+    
+    // Options not used in this Worker
+}
+
+#pragma mark - Single Item CRUD
+
+- (void)doLoadObjectForId:(nonnull NSString*)userDeviceId
+                andUserId:(nonnull NSString*)userId
+                withBlock:(nullable PTCLUserDeviceBlockVoidDAOUserDeviceNSErrorContinue)block
+           andUpdateBlock:(nullable PTCLUserDeviceBlockVoidDAOUserDeviceNSError)updateBlock
+{
+    if (self.nextUserDeviceWorker)
+    {
+        [self.nextUserDeviceWorker doLoadObjectForId:userDeviceId
+                                           andUserId:userId
+                                           withBlock:block
+                                      andUpdateBlock:updateBlock];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+- (void)doDeleteObject:(nonnull DAOUserDevice*)userDevice
+             withBlock:(nullable PTCLUserDeviceBlockVoidBOOLNSError)block
+{
+    if (self.nextUserDeviceWorker)
+    {
+        [self.nextUserDeviceWorker doDeleteObject:userDevice
+                                        withBlock:block];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+- (void)doDeleteObjectForId:(nonnull NSString*)userDeviceId
+                  andUserId:(nonnull NSString*)userId
+                  withBlock:(nullable PTCLUserDeviceBlockVoidBOOLNSError)block
+{
+    if (self.nextUserDeviceWorker)
+    {
+        [self.nextUserDeviceWorker doDeleteObjectForId:userDeviceId
+                                             andUserId:userId
+                                             withBlock:block];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+- (void)doSaveObject:(nonnull DAOUserDevice*)userDevice
+           withBlock:(nullable PTCLUserDeviceBlockVoidBOOLNSError)block
+{
+    if (self.nextUserDeviceWorker)
+    {
+        [self.nextUserDeviceWorker doSaveObject:userDevice
+                                      withBlock:block];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+#pragma mark - Collection Items CRUD
+
+- (void)doLoadObjectsForUser:(nonnull DAOUser*)user
+              withParameters:(nullable NSDictionary*)parameters
+                    andBlock:(nullable PTCLUserDeviceBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)block
+              andUpdateBlock:(nullable PTCLUserDeviceBlockVoidNSArrayNSUIntegerNSUIntegerNSError)updateBlock
+{
+    if (self.nextUserDeviceWorker)
+    {
+        [self.nextUserDeviceWorker doLoadObjectsForUser:user
+                                         withParameters:parameters
+                                               andBlock:block
+                                         andUpdateBlock:updateBlock];
+    }
+    
+    NSException*    exception = [NSException exceptionWithName:[NSString stringWithFormat:@"%@ Exception", NSStringFromClass(self.class)]
+                                                        reason:@"Crash worker should not be actually used!"
+                                                      userInfo:nil];
+    @throw exception;
+}
+
+@end
